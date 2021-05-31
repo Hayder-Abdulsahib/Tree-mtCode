5599fe2bc5831c32451971b392c1e011b2ab5138
Hayder-Abdulsahib <hyder.sahib96@gmail.com>
2021-05-31 17:16:19 +0300

    solved

--- tree-myCode.js
+++ tree-myCode.js
@@ -5,40 +5,64 @@ class TreeNode {
     constructor(name) {
         this.name = name;
         this.children = [];
-
     }
 
-    addChild = (node) => {
+
+
+    addChild = (child) => {
         if (this.children.length < 2) {
-            this.children.push(node);
-            console.log(`added a child to ${this.name}`)
+            this.children.push(child);
+            console.log(`child ${child.name} has been added`)
         }
         else console.log("child is an orphan")
     };
 
 
     traverse = () => {
-        let nodes = [this]; // to put the root node in our array
+        let nodes = [this];
         while (nodes.length > 0) {
             let currentNode = nodes.pop();
             console.log(currentNode.name);
             nodes = [...nodes, ...currentNode.children];
         }
     };
+
+    searchForParent = (child) => {
+        //zainab family => family bobo  
+        let nodes = [this];
+        while (nodes.length > 0) {
+            let currentNode = nodes.pop();
+            if (child.name.split(" ")[1] === currentNode.name.split(" ")[0]) {
+                return currentNode
+            }
+
+            nodes = [...nodes, ...currentNode.children];
+        }
+        return "Parent does not exist";
+    };
 }
 
 
-const root = new TreeNode("family");
-root.addChild("jack");
-
-const name = prompt("enter child full name (done if finished): ");
-console.log(`added a child to ${}`)
-
-const childOne = new TreeNode("Fahad");
-const childTwo = new TreeNode("Jassim");
 
 
-root.addChild(childOne);
-root.addChild(childTwo);
+let childName = prompt("enter child full name (Done if finished): ");
+const root = new TreeNode("family")
+while (childName.toUpperCase() !== "DONE") {
+    //create new child node
+    let child = new TreeNode(childName);
 
-console.log(root);
+    //look for its parent
+    let parent = root.searchForParent(child);
+
+    //find it and add child i childeren < 2
+    if (parent !== "Parent does not exist") {
+        //add the child
+        parent.addChild(child)
+    }
+    //else do not add the child
+    else console.log(parent)
+    childName = prompt("enter child full name (Done if finished): ")
+
+}
+
+root.traverse();
